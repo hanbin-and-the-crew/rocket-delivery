fe80f68fad361ed573c76862b9a7c2071949996d
parkg17 <parkg17@naver.com>
2025-11-04 22:17:44 +0900

    [FEAT] Eureka Server and Client 설정 및 Gateway 각 루트 설정

---
+++ module-user/src/main/java/org/sparta/user/presentation/controller/UserController.java
@@ -0,0 +1,14 @@
+package org.sparta.user.presentation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Company Service!";
+    }
+}
