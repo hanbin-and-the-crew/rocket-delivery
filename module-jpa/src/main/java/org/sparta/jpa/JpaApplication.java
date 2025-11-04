30057e1645f0436982874500244c2edb870ccfef
parkg17 <parkg17@naver.com>
2025-11-05 08:16:21 +0900

    [FEAT] Jpa module 추가 및 Gateway route 추가

---
+++ module-jpa/src/main/java/org/sparta/jpa/JpaApplication.java
@@ -0,0 +1,14 @@
+package org.sparta.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class JpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+
+}
