96e5ff55fb9da2e82fc77545fddf886acb62bb8d
Yu-Jin9 <ujh50188@gmail.com>
2025-11-13 09:29:24 +0900

    [BUILD] 환경 구성

---
+++ module-deliveryman/src/main/java/DeliveryManApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class DeliveryManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryManApplication.class, args);
+    }
+
+}
