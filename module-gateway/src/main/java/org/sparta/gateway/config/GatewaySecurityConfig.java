462dab715287d2d3b66ce3baff657f641f9ba0d1
parkg17 <parkg17@naver.com>
2025-11-27 10:04:57 +0900

    [FEATURE] 잠시 인가 과정 허용하도록
    개발끝나면 다시 막을 예정

--- module-gateway/src/main/java/org/sparta/gateway/config/GatewaySecurityConfig.java
+++ module-gateway/src/main/java/org/sparta/gateway/config/GatewaySecurityConfig.java
@@ -51,7 +51,8 @@ public class GatewaySecurityConfig {
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers(WHITE_LIST).permitAll()
                         .pathMatchers(HttpMethod.OPTIONS).permitAll()
-                        .anyExchange().authenticated()
+                        //.anyExchange().authenticated()
+                        .anyExchange().permitAll() // 프로젝트 마지막쯤에 다시 모든 요청에 대해 인가 과정 적용할 예정
                 )
 
                 // JWT 인증 필터 등록(가장 중요한 부분)
