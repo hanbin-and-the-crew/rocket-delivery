c1b615112286e113318c2c33676059a350739a5a
parkg17 <parkg17@naver.com>
2025-12-05 12:29:00 +0900

    [REFACTOR] User Service 중복 제거

--- module-user/src/main/java/org/sparta/user/application/service/UserService.java
+++ module-user/src/main/java/org/sparta/user/application/service/UserService.java
@@ -26,9 +26,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
 import java.util.UUID;
 
@@ -49,28 +47,26 @@ public class UserService {
     @CacheEvict(value = "userListCache", allEntries = true)
     public UserResponse.SignUpUser signup(UserCommand.SignUpUser request) {
 
-        String userName = request.userName();
-        String realName = request.realName();
-        String userPhoneNumber = request.userPhoneNumber();
-        String email = request.email();
-        String password = passwordEncoder.encode(request.password());
-        String slackId = request.slackId();
-        UUID hubId = request.hubId();
-        UserRoleEnum role = request.role();
-        DeliveryManagerRoleEnum deliveryManagerRoleEnum = request.deliveryRole();
-
         // 중복 체크: userName, email
-        userRepository.findByUserName(userName).ifPresent(u -> {
-            throw new BusinessException(UserErrorType.VALIDATION_FAILED, "중복된 사용자 ID가 존재합니다.");
-        });
-        userRepository.findByEmail(email).ifPresent(u -> {
-            throw new BusinessException(UserErrorType.VALIDATION_FAILED,"중복된 Email 입니다.");
-        });
+        String userName = request.userName();
+        String email = request.email();
+        validateDuplicateUser(userName, email);
+
+        // 비밀번호 인코딩
+        String password = passwordEncoder.encode(request.password());
 
         // 사용자 생성 및 저장
         User user = User.create(
-                userName, password, slackId, realName,
-                userPhoneNumber, email, role, hubId, deliveryManagerRoleEnum);
+                userName,
+                password,
+                request.slackId(),
+                request.realName(),
+                request.userPhoneNumber(),
+                email,
+                request.role(),
+                request.hubId(),
+                request.deliveryRole()
+        );
 
         user = userRepository.save(user);
 
@@ -85,7 +81,7 @@ public class UserService {
      */
     public UserResponse.GetUser getUserInfo(CustomUserDetails userDetailsInfo) {
         User user = userRepository.findByUserId(userDetailsInfo.getId())
-                .orElseThrow(() -> new BusinessException(UserErrorType.USER_NOT_FOUND, "회원이 존재하지 않습니다."));
+                .orElseThrow(() -> new BusinessException(UserErrorType.USER_NOT_FOUND));
         return UserResponse.GetUser.from(user);
     }
 
@@ -98,61 +94,24 @@ public class UserService {
             @CacheEvict(value = "userListCache", allEntries = true)
     })
     public UserResponse.UpdateUser updateSelf(CustomUserDetails user, UserCommand.UpdateUser request) {
-        User userInfo = userRepository.findByUserId(user.getId()).orElseThrow(
-                () -> new BusinessException(UserErrorType.UNAUTHORIZED,"수정할 유저 정보가 없습니다.")
-        );
-        if (userInfo.getDeletedAt() != null) {
-            throw new BusinessException(UserErrorType.UNAUTHORIZED," 탈퇴한 회원입니다.");
-        }
-        String newPassword = passwordEncoder.encode(request.newPassword().trim());
 
-        // JPA Dirty Checking으로 업데이트: 새 엔티티 생성/저장은 금지
-        if (request.userName() != null && !request.userName().isBlank()) {
-            userInfo.updateUserName(request.userName().trim());
-        }
-        // 비밀번호
-        if (passwordEncoder.matches(request.oldPassword(), user.getPassword()) && request.newPassword() != null && !request.newPassword().isBlank()) {
-            userInfo.updatePassword(newPassword);
-        }
-        // 이메일
-        if (request.email() != null && !request.email().isBlank()) {
-            userInfo.updateEmail(request.email().trim());
-        }
-        //전화번호
-        if (request.userPhoneNumber() != null && !request.userPhoneNumber().isBlank()) {
-            userInfo.updatePhoneNumber(request.userPhoneNumber().trim());
-        }
+        User userInfo = userRepository.findByUserId(user.getId())
+                .orElseThrow(() -> new BusinessException(UserErrorType.UNAUTHORIZED, "수정할 유저 정보가 없습니다."));
 
-        User updateUser = userRepository.save(userInfo);
+        User updated = updateUserInfo(userInfo, request);
 
-        /*
-        정보수정에 오류가 발생하여 기존 SpringSecurity에 저장된 SecurityContext 또한 수정을 해야한다는 것을 알게되어
-        기존 정보 호출
-         */
-        CustomUserDetails updatedUserDetails = customUserDetailsService.loadUserByUsername(updateUser.getUserName());
+        // 업데이트된 사용자 정보를 SecurityContext에 반영
+        CustomUserDetails updatedUserDetails = customUserDetailsService.loadUserByUsername(updated.getUserName());
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        /*
-        호출된 값을 setAuthentication를 사용하여 새 값으로 변경하고 최종 setContext하여 업데이트 처리
-         */
 
         SecurityContext context = SecurityContextHolder.getContext();
         context.setAuthentication(updateAuthentication(authentication, updatedUserDetails));
         SecurityContextHolder.setContext(context);
 
-        // 유저 정보 변경 이벤트 발행
-        eventPublisher.publishExternal(UserUpdatedEvent.of(updateUser));
+        // 이벤트 발행
+        eventPublisher.publishExternal(UserUpdatedEvent.of(updated));
 
-        return UserResponse.UpdateUser.from(updateUser);
-    }
-
-    // 업데이트 인증처리에 대한 추가적인 메서드를 작성하여 기존 인증값과 현재 업데이트 된 값을 가져와서 업데이트 처리
-    protected Authentication updateAuthentication(Authentication authentication, CustomUserDetails userDetails) {
-        UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(
-                userDetails, authentication.getCredentials(), userDetails.getAuthorities());
-        newAuth.setDetails(authentication.getDetails());
-
-        return newAuth;
+        return UserResponse.UpdateUser.from(updated);
     }
 
     /**
@@ -164,17 +123,15 @@ public class UserService {
             @CacheEvict(value = "userListCache", allEntries = true)
     })
     public void deleteSelf(CustomUserDetails userDetails) {
-        User user = userRepository.findByUserId(userDetails.getId())
-                .orElseThrow(() -> new BusinessException(UserErrorType.NOT_FOUND, "이미 탈퇴했거나 존재하지 않는 회원입니다."));
+        User userInfo = userRepository.findByUserId(userDetails.getId())
+                .orElseThrow(() -> new BusinessException(UserErrorType.NOT_FOUND));
 
-        LocalDateTime now = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
-        int updated = userRepository.softDeleteByUserId(user.getUserId(), now);
-        if (updated == 0) {
-            throw new BusinessException(UserErrorType.NOT_FOUND, "이미 탈퇴했거나 존재하지 않는 회원입니다.");
-        }
+        User deletedUser = softDeleteUser(userInfo);
 
-        // 유저 삭제 이벤트 발행
-        eventPublisher.publishExternal(UserDeletedEvent.of(user));
+        // 로그인 세션 완전 초기화
+        SecurityContextHolder.clearContext();
+
+        eventPublisher.publishExternal(UserDeletedEvent.of(deletedUser));
     }
 
     /**
@@ -198,12 +155,12 @@ public class UserService {
     @Cacheable(value = "userCache", key = "#userId")
     public UserResponse.GetUser getSpecificUserInfo(UUID userId) {
         User user = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new BusinessException(UserErrorType.USER_NOT_FOUND, "회원이 존재하지 않습니다."));
+                .orElseThrow(() -> new BusinessException(UserErrorType.USER_NOT_FOUND));
         return UserResponse.GetUser.from(user);
     }
 
     /**
-     * PATCH /users/me
+     * PATCH users/bos/{userId}
      */
     @Transactional
     @Caching(evict = {
@@ -211,52 +168,15 @@ public class UserService {
             @CacheEvict(value = "userListCache", allEntries = true)
     })
     public UserResponse.UpdateUser updateUser(UUID userId, UserCommand.UpdateUser request) {
-        User userInfo = userRepository.findByUserId(userId).orElseThrow(
-                () -> new BusinessException(UserErrorType.UNAUTHORIZED,"수정할 유저 정보가 없습니다.")
-        );
-        if (userInfo.getDeletedAt() != null) {
-            throw new BusinessException(UserErrorType.UNAUTHORIZED," 탈퇴한 회원입니다.");
-        }
-        String newPassword = passwordEncoder.encode(request.newPassword().trim());
 
-        // JPA Dirty Checking으로 업데이트: 새 엔티티 생성/저장은 금지
-        if (request.userName() != null && !request.userName().isBlank()) {
-            userInfo.updateUserName(request.userName().trim());
-        }
-        // 비밀번호
-        if (passwordEncoder.matches(request.oldPassword(), userInfo.getPassword()) && request.newPassword() != null && !request.newPassword().isBlank()) {
-            userInfo.updatePassword(newPassword);
-        }
-        // 이메일
-        if (request.email() != null && !request.email().isBlank()) {
-            userInfo.updateEmail(request.email().trim());
-        }
-        //전화번호
-        if (request.userPhoneNumber() != null && !request.userPhoneNumber().isBlank()) {
-            userInfo.updatePhoneNumber(request.userPhoneNumber().trim());
-        }
+        User userInfo = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new BusinessException(UserErrorType.UNAUTHORIZED, "수정할 유저 정보가 없습니다."));
 
-        User updateUser = userRepository.save(userInfo);
+        User updated = updateUserInfo(userInfo, request);
 
-        /*
-        정보수정에 오류가 발생하여 기존 SpringSecurity에 저장된 SecurityContext 또한 수정을 해야한다는 것을 알게되어
-        기존 정보 호출
-         */
-        CustomUserDetails updatedUserDetails = customUserDetailsService.loadUserByUsername(updateUser.getUserName());
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        eventPublisher.publishExternal(UserUpdatedEvent.of(updated));
 
-        /*
-        호출된 값을 setAuthentication를 사용하여 새 값으로 변경하고 최종 setContext하여 업데이트 처리
-         */
-
-        SecurityContext context = SecurityContextHolder.getContext();
-        context.setAuthentication(updateAuthentication(authentication, updatedUserDetails));
-        SecurityContextHolder.setContext(context);
-
-        // 유저 정보 변경 이벤트 발행
-        eventPublisher.publishExternal(UserUpdatedEvent.of(updateUser));
-
-        return UserResponse.UpdateUser.from(updateUser);
+        return UserResponse.UpdateUser.from(updated);
     }
 
     /**
@@ -268,17 +188,12 @@ public class UserService {
             @CacheEvict(value = "userListCache", allEntries = true)
     })
     public void deleteUser(UUID userId) {
-        User user = userRepository.findByUserId(userId)
+        User userInfo = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new BusinessException(UserErrorType.NOT_FOUND));
 
-        LocalDateTime now = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
-        int updated = userRepository.softDeleteByUserId(user.getUserId(), now);
-        if (updated == 0) {
-            throw new BusinessException(UserErrorType.NOT_FOUND);
-        }
+        User deletedUser = softDeleteUser(userInfo);
 
-        // 유저 삭제 이벤트 발행
-        eventPublisher.publishExternal(UserDeletedEvent.of(user));
+        eventPublisher.publishExternal(UserDeletedEvent.of(deletedUser));
     }
 
     /**
@@ -300,7 +215,7 @@ public class UserService {
     @CacheEvict(value = "userListCache", allEntries = true)
     public void updateUserStatus(UUID userId, UserStatusEnum newStatus) {
         User user = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new BusinessException(UserErrorType.USER_NOT_FOUND, "회원이 존재하지 않습니다."));
+                .orElseThrow(() -> new BusinessException(UserErrorType.USER_NOT_FOUND));
 
         if (user.getStatus() != UserStatusEnum.PENDING) {
             throw new BusinessException(UserErrorType.INVALID_STATUS_CHANGE, "대기중인 회원만 상태를 변경할 수 있습니다.");
@@ -312,5 +227,75 @@ public class UserService {
         eventPublisher.publishExternal(UserRoleChangedEvent.of(user));
     }
 
+    /**
+     * 공통 메서드
+     */
+    private void validateDuplicateUser(String userName, String email) {
+        userRepository.findByUserName(userName).ifPresent(u -> {
+            throw new BusinessException(UserErrorType.VALIDATION_FAILED, "중복된 사용자 ID가 존재합니다.");
+        });
 
+        userRepository.findByEmail(email).ifPresent(u -> {
+            throw new BusinessException(UserErrorType.VALIDATION_FAILED, "중복된 Email 입니다.");
+        });
+    }
+
+    private User updateUserInfo(User userInfo, UserCommand.UpdateUser request) {
+
+        if (userInfo.getDeletedAt() != null) {
+            throw new BusinessException(UserErrorType.USER_ALREADY_WITHDRAW);
+        }
+
+        String newPassword = request.newPassword() != null && !request.newPassword().isBlank()
+                ? passwordEncoder.encode(request.newPassword().trim())
+                : null;
+
+        // 이름
+        if (request.userName() != null && !request.userName().isBlank()) {
+            userInfo.updateUserName(request.userName().trim());
+        }
+
+        // 비밀번호
+        if (request.oldPassword() != null
+                && newPassword != null
+                && passwordEncoder.matches(request.oldPassword(), userInfo.getPassword())) {
+            userInfo.updatePassword(newPassword);
+        }
+
+        // 이메일
+        if (request.email() != null && !request.email().isBlank()) {
+            userInfo.updateEmail(request.email().trim());
+        }
+
+        // 전화번호
+        if (request.userPhoneNumber() != null && !request.userPhoneNumber().isBlank()) {
+            userInfo.updatePhoneNumber(request.userPhoneNumber().trim());
+        }
+
+        return userRepository.save(userInfo);
+    }
+
+    private User softDeleteUser(User user) {
+        if (user.getDeletedAt() != null) {
+            throw new BusinessException(UserErrorType.USER_ALREADY_WITHDRAW);
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        int updated = userRepository.softDeleteByUserId(user.getUserId(), now);
+
+        if (updated == 0) {
+            throw new BusinessException(UserErrorType.USER_NOT_FOUND);
+        }
+
+        return user; // 이벤트 발행용
+    }
+
+    // 업데이트 인증처리에 대한 추가적인 메서드를 작성하여 기존 인증값과 현재 업데이트 된 값을 가져와서 업데이트 처리
+    protected Authentication updateAuthentication(Authentication authentication, CustomUserDetails userDetails) {
+        UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(
+                userDetails, authentication.getCredentials(), userDetails.getAuthorities());
+        newAuth.setDetails(authentication.getDetails());
+
+        return newAuth;
+    }
 }
