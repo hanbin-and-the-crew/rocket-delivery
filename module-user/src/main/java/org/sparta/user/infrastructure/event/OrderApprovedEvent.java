a2acdcf21968c6d7193022b5572199933d116ccc
parkg17 <parkg17@naver.com>
2025-12-09 15:58:47 +0900

    [Fix] User쪽 OrderApprovedEvent 수정

--- module-user/src/main/java/org/sparta/user/infrastructure/event/OrderApprovedEvent.java
+++ module-user/src/main/java/org/sparta/user/infrastructure/event/OrderApprovedEvent.java
@@ -4,9 +4,18 @@ import java.time.Instant;
 import java.util.UUID;
 
 public record OrderApprovedEvent(
-        UUID eventId,       // 멱등성 보장용
+        UUID eventId,
+        Instant occurredAt,
         UUID orderId,
-        UUID userId,
-        Instant occurredAt
+        UUID customerId,
+        UUID receiveHubId,
+        UUID receiveCompanyId,
+        UUID supplierHubId,
+        UUID supplierCompanyId,
+        String address,
+        String receiverPhone,
+        String dueAt,
+        String requestedMemo,
+        String receiverSlackId
 ) {
 }
