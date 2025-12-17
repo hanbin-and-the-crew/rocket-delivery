0f482913e7c10ea8eaf75f31debd82ef83e5b12a
hellohanbi <dearhaan@gmail.com>
2025-12-17 16:43:34 +0900

    클래스 이름 오타 수정

--- module-product/src/test/java/org/sparta/product/domain/event/StockConfirmedEventTest.java
+++ module-product/src/test/java/org/sparta/product/domain/event/StockConfirmedEventTest.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StockConfirmEventTest {
+class StockConfirmedEventTest {
 
     @Test
     @DisplayName("ProductOutboxEvent.stockConfirmed: aggregate/eventType/status 세팅")
